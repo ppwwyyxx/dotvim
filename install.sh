30a4ad153eb7093aa2f24146f7df51718d6826f1
ppwwyyxx <ppwwyyxxc@gmail.com>
2015-06-27 22:47:20 +0800

    remove dict

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # File: install.sh
-# Date: Mon Mar 31 20:08:58 2014 +0800
+# Date: Sat Jun 27 22:46:46 2015 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 echo "Backup original vimfiles to ~/backup_vim* ..."
 [[ -f ~/.vimrc ]] && mv ~/.vimrc ~/backup_vimrc -v
@@ -12,14 +12,17 @@ echo "Copying files..."
 cp vimrc ~/.vimrc
 rm ~/.vim -rvf && cp vim ~/.vim -rvf
 
+echo "Generating dict..."
+cd ~/.vim/static/
+python dict_to_cases.py
+
+mkdir -p ~/.vimtmp/undo
+mkdir -p ~/.vimtmp/vim-fuf-data
+
 echo "Installing Bundles ..."
 mkdir -p ~/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
-
-mkdir -p ~/.vimtmp/undo
-mkdir -p ~/.vimtmp/vim-fuf-data
-mkdir -p ~/.vimtmp/neocomplcache
 
 for i in ~/.vim/patch/*; do
    filename=`basename $i`
@@ -43,15 +46,15 @@ else
 	cd && rm $BUILD_DIR -rf
 fi
 
-VIMPROC_DIR="bundle/vimproc/autoload"
-if [[ -f ~/backup_vim/$VIMPROC_DIR/vimproc_unix.so ]]; then
-	cp ~/backup_vim/$VIMPROC_DIR/*.so ~/.vim/$VIMPROC_DIR
-else
-	echo "Compiling vimproc..."
-	echo $LINE
-	cd ~/.vim/$VIMPROC_DIR/..
-	make -f make_unix.mak
-fi
+#VIMPROC_DIR="bundle/vimproc/autoload"
+#if [[ -f ~/backup_vim/$VIMPROC_DIR/vimproc_unix.so ]]; then
+	#cp ~/backup_vim/$VIMPROC_DIR/*.so ~/.vim/$VIMPROC_DIR
+#else
+	#echo "Compiling vimproc..."
+	#echo $LINE
+	#cd ~/.vim/$VIMPROC_DIR/..
+	#make -f make_unix.mak
+#fi
 
 # install eclim
 pacaur -S eclim
